76e0dd7b7886617c7cdae3865e41aa47222c2cda
Deyvison J Paula <deyvisonjp@id.uff.br>
2020-07-27 22:37:52 -0300

    Corrigindo cores e mudando banner

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"Imersão Alura"}
+        videoDescription={"Imersão React"}
       />
 
       <Carousel
